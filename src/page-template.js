94300ff89bb216e16e8a9ffadd2455af988095a2
Zach Oda <zjodachow@gmail.com>
2022-08-12 14:47:10 -0400

    CSS improvements

--- src/page-template.js
+++ src/page-template.js
@@ -1,26 +1,31 @@
 const generateManager = (Manager) => {
   console.log(Manager.name);
   return `
-        <div class="card">
+        <div class="card text-bg-light mb-3">
+        <div class="card-header"</div>
         <div class="card-body">
         <h1 class="card-title">${Manager.name}; Manager</h1>
-        <p> Employee ID: ${Manager.id}</p>
-        <p><a href="mailto:${Manager.email}"> Email:${Manager.email}</a></p>
-        <p> Office Number: ${Manager.officeNumber}</p>
+        <p class="card-text"> Employee ID: ${Manager.id}</p>
+        <p class="card-text"><a href="mailto:${Manager.email}"> Email:${Manager.email}</a></p>
+        <p class="card-text"> Office Number: ${Manager.officeNumber}</p>
+        </div>
+        </div>
     `;
 };
 
 const generateEngineer = (engineerArray) => {
-  let returnValue = '';
+  let returnValue = "";
   console.log(engineerArray);
-  engineerArray.forEach(engineer => {
-    returnValue = returnValue + `
-          <div class="card">
+  engineerArray.forEach((engineer) => {
+    returnValue =
+      returnValue +
+      `
+          <div class="card text-bg-light mb-3">
+          <h1 class="card-header">${engineer.name}; Engineer</h1>
           <div class="card-body">
-          <h1 class="card-title">${engineer.name}; Engineer</h1>
-          <p> Employee ID: ${engineer.id}</p>
-          <p><a href="mailto:${engineer.email}"> Email:${engineer.email}</a></p>
-          <p><a href="github.com/${engineer.github}"> Github: ${engineer.github}</a></p>
+          <p class="card-text"> Employee ID: ${engineer.id}</p>
+          <p class="card-text"><a href="mailto:${engineer.email}"> Email:${engineer.email}</a></p>
+          <p class="card-text"><a href="github.com/${engineer.github}"> Github: ${engineer.github}</a></p>
           </div>
           </div>
           `;
@@ -29,21 +34,22 @@ const generateEngineer = (engineerArray) => {
 };
 
 const generateIntern = (internArray) => {
-  let returnValue = '';
-  internArray.forEach(intern => {
-    returnValue = returnValue +
-   `
-        <div class="card">
+  let returnValue = "";
+  internArray.forEach((intern) => {
+    returnValue =
+      returnValue +
+      `
+        <div class="card text-bg-light mb-3">
+        <h1 class="card-header">${intern.name}; Intern</h1>
         <div class="card-body">
-        <h1 class="card-title">${intern.name}; Intern</h1>
-        <p> Employee ID: ${intern.id}</p>
-        <p><a href="mailto:${intern.email}"> Email:${intern.email}</a></p>
-        <p> School: ${intern.school}</p>
+        <p class="card-text"> Employee ID: ${intern.id}</p>
+        <p class="card-text"><a href="mailto:${intern.email}"> Email:${intern.email}</a></p>
+        <p class="card-text"> School: ${intern.school}</p>
         </div>
         </div>
         `;
-      });
-      return returnValue;
+  });
+  return returnValue;
 };
 
 module.exports = (managerValue, engineerArray, internArray) => {
@@ -56,9 +62,8 @@ module.exports = (managerValue, engineerArray, internArray) => {
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
       <title>Portfolio Demo</title>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-      <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
-      <link rel="stylesheet" href="style.css">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css
+      ">
     </head>
     <body>
      <div class= "container">
@@ -69,5 +74,4 @@ module.exports = (managerValue, engineerArray, internArray) => {
      </body>
      </html>
      `;
-    };
-    
+};
